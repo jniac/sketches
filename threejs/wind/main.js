bf2db2c3d8a879ed1dcd20d7e84bd624ca7a93ea
jniac <joseph.merdrignac@gmail.com>
2022-10-18 00:53:54 +0200

    ok, not so easy

--- threejs/wind/main.js
+++ threejs/wind/main.js
@@ -24,8 +24,16 @@ const createGround = () => {
 }
 
 const createFoliage = () => {
+  const injectBefore = (source, pattern, injectedCode) => {
+    return source.replace(pattern, `${injectedCode}\n${pattern}`)
+  }
+  const injectAfter = (source, pattern, injectedCode) => {
+    return source.replace(pattern, `${pattern}\n${injectedCode}`)
+  }
+
   const geometry = new THREE.PlaneGeometry()
   geometry.translate(0, 0.5, 0)
+
   const material = new THREE.MeshPhysicalMaterial({
     map: loadTexture("assets/foliage.png"),
     alphaMap: loadTexture("assets/foliage-alpha.png"),
@@ -33,12 +41,46 @@ const createFoliage = () => {
     transparent: true,
     side: THREE.DoubleSide,
   })
-  const plane = new THREE.Mesh(geometry, material)
-  plane.castShadow = true
-  scene.add(plane)
+
+  let shader = null
+  material.onBeforeCompile = _shader => {
+    shader = _shader
+    shader.uniforms.uTime = { value: 0 }
+    shader.uniforms.uWindDirection = { value: new THREE.Vector3(1, 0, 1) }
+    shader.uniforms.uWindStrength = { value: .1 }
+    shader.vertexShader = injectBefore(shader.vertexShader, '#include <common>', `
+      uniform float uTime;
+      uniform vec3 uWindDirection;
+      uniform float uWindStrength;
+    `)
+    // https://github.com/mrdoob/three.js/blob/master/src/renderers/shaders/ShaderChunk/project_vertex.glsl.js
+    shader.vertexShader = shader.vertexShader.replace('#include <project_vertex>', `
+      vec4 mvPosition = vec4( transformed, 1.0 );
+      #ifdef USE_INSTANCING
+        mvPosition = instanceMatrix * mvPosition;
+      #endif
+      mvPosition = modelMatrix * mvPosition;
+      mvPosition.xyz += uWindDirection * vUv.y * sin(uTime) * uWindStrength;
+      mvPosition = viewMatrix * mvPosition;
+      gl_Position = projectionMatrix * mvPosition;
+    `)
+  }
+
+  const mesh = new THREE.Mesh(geometry, material)
+  mesh.castShadow = true
+  mesh.onBeforeRender = () => {
+    if (shader) {
+      shader.uniforms.uTime.value += 1 / 60
+    }
+  }
+  scene.add(mesh)
+
+  return mesh
 }
 
 initShadow()
 createGrid()
 createGround()
 createFoliage()
+createFoliage().rotation.set(0, Math.PI * .25, 0)
+createFoliage().rotation.set(0, Math.PI * -.25, 0)
